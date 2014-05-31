ea3ec55d6fd6ff4fca504b0a3fda7566999b5a34
Doug Bromley <doug.bromley@gmail.com>
2014-05-31 09:31:32 +0100

    Added lots more tests for file contents and ncontents dependent on choices. Fixes #1

--- test/test.js
+++ test/test.js
@@ -13,6 +13,8 @@ describe('gruntfile-yii generator', function () {
       this.app = helpers.createGenerator('gruntfile-yii:app', [
         '../../app'
       ]);
+      this.app.options['skip-install'] = true;
+      this.app.options['skip-install-message'] = true;
       done();
     }.bind(this));
   });
@@ -21,39 +23,6 @@ describe('gruntfile-yii generator', function () {
     var expected = ['Gruntfile.js', 'package.json'];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
-    });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFile(expected);
-      done();
-    });
-  });
-});
------------------
-var file = require('yeoman-generator').file;
-var helpers = require('yeoman-generator').test;
-var path = require('path');
-
-describe('gruntfile:app empty project', function () {
-  var generator;
-
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        done(err);
-      }
-      generator = helpers.createGenerator('gruntfile:app', ['../../app']);
-      generator.options['skip-install'] = true;
-      generator.options['skip-install-message'] = true;
-      done();
-    });
-  });
-
-  it('creates expected files', function (done) {
-    var expected = ['Gruntfile.js', 'package.json'];
-
-    helpers.mockPrompt(generator, {
       phpTools: [
         'phpCs',
         'phpMd',
@@ -62,9 +31,66 @@ describe('gruntfile:app empty project', function () {
       ]
     });
 
-    generator.run({}, function () {
+    this.app.run({}, function () {
       helpers.assertFile(expected);
-      helpers.assertFileContent('./package.json', /"version": "0.0.0"/);
+      helpers.assertFileContent('./Gruntfile.js', /grunt\.loadNpmTasks\('grunt-php'\)/);
+      helpers.assertFileContent('./package.json', /"grunt-php": "~0.3.3"/);
+      done();
+    });
+  });
+
+  it('package.json contain all PHPTools', function (done) {
+    helpers.mockPrompt(this.app, {
+      phpTools: [
+        'phpCs',
+        'phpMd',
+        'phpCpd',
+        'phpCsFixer'
+      ]
+    });
+
+    this.app.run({}, function () {
+      helpers.assertFileContent('./package.json', /"grunt-php": "~0.3.3"/);
+      helpers.assertFileContent('./package.json', /"grunt-phpcs": "~0.2.2"/);
+      helpers.assertFileContent('./package.json', /"grunt-phpmd": "~0.1.0"/);
+      helpers.assertFileContent('./package.json', /"grunt-phpcpd": "~0.1.3"/);
+      helpers.assertFileContent('./package.json', /"grunt-php-cs-fixer": "~0.0.5"/);
+      done();
+    });
+  });
+
+  it('package.json contains selected PHPTools', function (done) {
+    helpers.mockPrompt(this.app, {
+      phpTools: [
+        'phpCs',
+        'phpCpd',
+      ]
+    });
+
+    this.app.run({}, function () {
+      helpers.assertFileContent('./package.json', /"grunt-php": "~0.3.3"/);
+      helpers.assertFileContent('./package.json', /"grunt-phpcs": "~0.2.2"/);
+      helpers.assertNoFileContent('./package.json', /"grunt-phpmd": "~0.1.0"/);
+      helpers.assertFileContent('./package.json', /"grunt-phpcpd": "~0.1.3"/);
+      helpers.assertNoFileContent('./package.json', /"grunt-php-cs-fixer": "~0.0.5"/);
+      done();
+    });
+  });
+
+  it('Gruntfile.js contains selected PHPTools', function (done) {
+    helpers.mockPrompt(this.app, {
+      phpTools: [
+        'phpCs',
+        'phpCpd',
+      ]
+    });
+
+    this.app.run({}, function () {
+      helpers.assertFileContent('./Gruntfile.js', /grunt.registerTask\('default', \['phplint:all'\]\)/);
+      helpers.assertFileContent('./Gruntfile.js', /grunt.loadNpmTasks\('grunt-phpcs'\)/);
+      helpers.assertNoFileContent('./Gruntfile.js', /grunt.loadNpmTasks\('grunt-phpmd'\)/);
+      helpers.assertNoFileContent('./Gruntfile.js', /grunt.loadNpmTasks\('grunt-php-cs-fixer'\)/);
+      helpers.assertFileContent('./Gruntfile.js', /grunt.loadNpmTasks\('grunt-phpcpd'\)/);
       done();
     });
   });
